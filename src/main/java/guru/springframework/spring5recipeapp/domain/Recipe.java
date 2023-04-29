e34fc100d2f2afae4cb9e2021bfe81906c88738f
Alexandros Fiouzi <fiouzi.alexandros@gmail.com>
2023-04-29 20:36:16 +0300

    display recipe and all of his properties

--- src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -1,9 +1,6 @@
 package guru.springframework.spring5recipeapp.domain;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.HashSet;
