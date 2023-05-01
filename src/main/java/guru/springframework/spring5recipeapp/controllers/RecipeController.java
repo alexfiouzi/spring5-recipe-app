e1c44b93641acd8573f948cf47cb5e961ff6fbc1
Alexandros Fiouzi <fiouzi.alexandros@gmail.com>
2023-05-01 13:50:44 +0300

    adding delete functionality

--- src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -2,41 +2,56 @@ package guru.springframework.spring5recipeapp.controllers;
 
 import guru.springframework.spring5recipeapp.commands.RecipeCommand;
 import guru.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
-   private final  RecipeService recipeService;
+    private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
+
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
-    public String showRecipeById(@PathVariable String id, Model model){
-        model.addAttribute("recipe",recipeService.findById(new Long(id)));
+    public String showRecipeById(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
     }
-@RequestMapping("recipe/new")
-    public String newRecipe(Model model){
-        model.addAttribute("recipe",new RecipeCommand());
+
+    @GetMapping
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
 
         return "recipe/recipeForm";
     }
 
+    @GetMapping
     @RequestMapping("recipe/{id}/update")
-    public String update(@PathVariable String id,Model model){
-        model.addAttribute("recipe",recipeService.findCommandById(Long.valueOf(id)));
+    public String update(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return "recipe/recipeForm";
     }
 
     @PostMapping
     @RequestMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
-    RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
-      return "redirect:/recipe/"+ savedCommand.getId() + "/show";
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id) {
+        log.debug("deleting id is " + id);
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
     }
 }
